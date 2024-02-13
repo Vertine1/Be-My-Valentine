61d61b08e9125dfb1c9c4569454a74bbf40ff74f
Vertine1 <verthennys@gmail.com>
2024-02-13 22:48:19 +0300

    Added my files

---
+++ Bemy.js
@@ -0,0 +1,7 @@
+// document.getElementById("no-button").addEventListener("mouseover", function() {
+//     // This  function Moves the button away from the cursor
+//     var newX = Math.floor(Math.random() * window.innerWidth);
+//     var newY = Math.floor(Math.random() * window.innerHeight);
+//     document.getElementById("no-button").style.left = newX + "px";
+//     document.getElementById("no-button").style.top = newY + "px";
+//   });
